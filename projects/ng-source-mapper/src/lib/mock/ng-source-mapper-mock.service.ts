4246151629dbe629ba54ae983e7c26f09083382f
John Barrett <john.barrett@polarisalpha.com>
2019-08-01 14:56:13 -0400

    add mock for source mapper, update angular version

---
+++ projects/ng-source-mapper/src/lib/mock/ng-source-mapper-mock.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { NgPosition } from '../models/ng-position';
+import { NgSourceMap } from '../models/ng-source-map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NgSourceMapper {
+
+  constructor() {}
+
+  /*
+	  Mock Public Functions
+	 */
+  public getSourceInfoFromMap(map: NgSourceMap, position: NgPosition): NgPosition {
+    return new NgPosition('mock.js', 0, 0);
+  }
+
+  public getSourceInfoFromURL(position: NgPosition, cache: boolean = true): Observable<NgPosition>{
+    return of(new NgPosition('mock.js', 0, 0));
+  }
+
+}
