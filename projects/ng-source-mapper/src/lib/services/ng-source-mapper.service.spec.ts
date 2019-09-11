4c60c17dbeb421daacdfba9bd5b64074ea7c4a07
John Barrett <john.barrett@polarisalpha.com>
2019-09-11 15:29:43 -0400

    update peer dependencies for angular 7+

--- projects/ng-source-mapper/src/lib/services/ng-source-mapper.service.spec.ts
+++ projects/ng-source-mapper/src/lib/services/ng-source-mapper.service.spec.ts
@@ -1,10 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClient } from '@angular/common/http';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 import { NgSourceMapper} from './ng-source-mapper.service';
 
 describe('NGSourceMapper', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    providers: [HttpClient]
+    imports: [HttpClientTestingModule]
   }));
 
   it('should be created', () => {
